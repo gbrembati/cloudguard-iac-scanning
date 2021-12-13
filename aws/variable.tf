9ecc0f37608cdca82423ed8c5a5c758a37678f6c
GiorgioBrembati <giorgio.brembati@gmail.com>
2021-12-13 18:05:23 +0100

    first commit

--- aws/variable.tf
+++ aws/variable.tf
@@ -1,13 +1,3 @@
 variable region {
   default = "us-east-1"
 }
-variable aws-access-key {
-    description = "AWS Access Key"
-    type = string
-    sensitive = true
-}
-variable aws-secret-key {
-    description = "AWS Secret Key"
-    type = string
-    sensitive = true
-}
